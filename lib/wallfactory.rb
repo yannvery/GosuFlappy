52d6fb8a0cd2fb50f4d94908b6734638b54551f9
Yann VERY <yann.very@gmail.com>
2014-11-11 00:06:50 +0100

    Add some docs and improve syntax

--- lib/wallfactory.rb
+++ lib/wallfactory.rb
@@ -9,24 +9,25 @@ class WallFactory
     @intervall = 175
   end
 
+  # Add a 2 walls separate by a standard height
   def add
-    wall_down = Wall.new(@window,"down")
-    wall_up = Wall.new(@window,"up")
+    wall_down = Wall.new(@window, 'down')
+    wall_up = Wall.new(@window, 'up')
     wall_up.y = wall_down.y - wall_up.height - @offset_y
-    @window.entities.push(wall_down,wall_up)
-    @window.walls.push(wall_down,wall_up)
+    @window.entities.push(wall_down, wall_up)
+    @window.walls.push(wall_down, wall_up)
   end
 
   def draw
   end
 
+  # Add a wall for each intervall
   def update
-    if (@counter >= @intervall)
+    if @counter >= @intervall
       self.add
       @counter = 0
     else
       @counter += 1
-      end
+    end
   end
-
 end
